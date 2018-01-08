f557cd1c54ebb2f82fa088808577254264dad923
yy282371715 <yy282371715@sina.cn>
2018-01-08 18:18:16 +0800

    update

--- dist/js/common.js
+++ dist/js/common.js
@@ -65,10 +65,6 @@ function ajax(options){
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-/**
- * Read the JavaScript cookies tutorial at:
- *   http://www.netspade.com/articles/javascript/cookies.xml
- */
 
 /**
  * Sets a Cookie with the given name and value.
@@ -81,11 +77,15 @@ function ajax(options){
  *              (default: domain of calling document)
  * [secure]   Boolean value indicating if the cookie transmission requires a
  *              secure transmission
+ *
+ * @param
+ * setCookie("tasty","strawberry2");
+ * setCookie("yummy","choco2",getData('s3'));
  */
 function setCookie(name, value, expires, path, domain, secure)
 {
     document.cookie= name + "=" + escape(value) +
-        ((expires) ? "; expires=" + expires.toGMTString() : "") +
+        ((expires) ? "; expires=" + expires.toUTCString() : "") +
         ((path) ? "; path=" + path : "") +
         ((domain) ? "; domain=" + domain : "") +
         ((secure) ? "; secure" : "");
@@ -98,6 +98,9 @@ function setCookie(name, value, expires, path, domain, secure)
  *
  * Returns a string containing value of specified cookie,
  *   or null if cookie does not exist.
+ *
+ * @param
+ * console.log(getCookie('tasty'));
  */
 function getCookie(name)
 {
@@ -127,6 +130,8 @@ function getCookie(name)
  * name      name of the cookie
  * [path]    path of the cookie (must be same as path used to create cookie)
  * [domain]  domain of the cookie (must be same as domain used to create cookie)
+ *
+ * @param deleteCookie('tasty');
  */
 function deleteCookie(name, path, domain)
 {
@@ -139,83 +144,27 @@ function deleteCookie(name, path, domain)
     }
 }
 
-
-/**
- * 自写cookie相关方法
+/*
+ * 获取想要的时间
+ * @param {string} str s1一秒 h1一小时 d1一天
+ * @return {number} 当前时间+str的时间
  * */
-(function(){
-
-    /*
-     * 获取cookie值
-     * @param {string} name - 要获取的cookie的name
-     * @retrun {string|''}
-     * */
-    function getCookie(name)
-    {
-        if (document.cookie.length>0)
-        {
-            var c_start=document.cookie.indexOf(name + "=");
-            if (c_start!=-1)
-            {
-                c_start=c_start + name.length+1
-                var c_end=document.cookie.indexOf(";",c_start)
-                if (c_end==-1)c_end=document.cookie.length;
-                return unescape(document.cookie.substring(c_start,c_end));
-            }
-        }
-        return "";
+function getData(str){
+    var str1=str.substring(0,1);
+    var str2=str.substring(1,str.length)*1;
+    var time=0;
+    if(str1=='s'){
+        time=str2*1000;
+    }else if(str1=='h'){
+        time=str2*60*60*1000;
+    }else if(str1=='d'){
+        time=str2*24*60*60*1000;
     }
+    var data=new Date();
+    data.setTime(data.valueOf()+time);
+    return data;
+}
 
-    /*
-     * 设置cookie
-     * @param {string} name - 设置cookie的name
-     * @param {value} value - 设置cookie的值
-     * @param {number|string} time - 0时不设定过期时间、's20'=20秒、'h12'=12小时、'd30'=30天
-     * */
-    function setCookie(name,value,time){
-        var cookieString=name+'='+escape(value);
-        if(time!=0){
-            var strsec=getsec(time);
-            var exp=new Date();
-            exp.setTime(exp.getTime()+strsec*1);
-            cookieString+=';expires='+exp.toGMTString();
-        }
-        document.cookie=cookieString;
-    }
-
-    /*
-     * 时间转换
-     * @param {string} time - 转换的时间
-     * @return {number} 转换时间后的毫秒值
-     * */
-    function getsec(time){
-        var str1=time.substring(0,1);
-        var str2=time.substring(1,time.length)*1;
-        if(str1=='s'){
-            return str2*1000;
-        }else if(str1=='h'){
-            return str2*60*60*1000;
-        }else if(str1=='d'){
-            return str2*24*60*60*1000;
-        }
-    }
-
-    /*
-     * 删除cookie
-     * @param {string} name - 删除cookie的name
-     * */
-    function delCookie(name){
-        var exp = new Date();
-        exp.setTime(exp.getTime() - 1);
-        var cval=getCookie(name);
-        if(cval!=null)
-            document.cookie= name + "="+cval+";expires="+exp.toGMTString();
-    }
-
-    //setCookie("tasty","strawberry2",0);
-    //setCookie("yummy","choco2",'s2');
-    //delCookie('tasty');
-}());
 /*
  * 数学相关的功能
  */
@@ -306,6 +255,30 @@ function deleteCookie(name, path, domain)
 				y:Math.cos(Math.PI/180*angle)*(Math.random()*randiusY)
 			}
         },
+
+        /**
+		 * 计算斐波那契数列 递归
+         */
+        fbnq:function(n){
+			if(n==1||n==2){
+				return 1;
+			}
+			return Math.fbnq(n-1)+Math.fbnq(n-2);
+		},
+
+        /**
+         * 计算斐波那契数列 非递归
+         */
+		fbnq2:function(n){
+            var a,b,res;
+            a = b = 1;
+            for(var i=3;i<=n;i++){
+                res = a + b;
+                a = b;
+                b = res;
+            }
+            return res;
+		}
 	};
 
 	Object.assign(Math,mathutils);
