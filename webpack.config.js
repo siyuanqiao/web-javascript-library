64758b364dfcb6b0e66f28d3124cec364d5a2018
bin.zeng <yy282371715@sina.cn>
2018-06-11 19:51:22 +0800

    webpack 创建 library

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack=require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+        filename: 'lab.js',
+        library: 'LAB',
+        libraryTarget:'var'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [path.resolve(__dirname,'src')]
+            }
+        ]
+    },
+    resolve: {
+
+    },
+    devtool: 'inline-source-map'
+};
