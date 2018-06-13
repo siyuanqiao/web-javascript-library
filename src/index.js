eaf58af228c1a2ef6cd81984c5c61c677f902ddd
bin.zeng <yy282371715@sina.cn>
2018-06-13 18:06:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-//基本数据类型：Nndefined Null Boolean String Number
-//引用数据类型：Object Symbol
-
 import './base/Array.js';
 import './base/Number.js';
 import './base/Math.js';
 
-import {setCookie,getCookie,deleteCookie} from "./cookies.js";
+export * from "./cookies.js";
 
-import OS from './OS.js';
-import Util from './Util.js';
-import URL from './URL.js';
-import Texture from "./Texture.js";
-import WXShare from "./WXShare.js";
-
-export {
-    OS,
-    Util,
-    URL,
-
-    setCookie,
-    getCookie,
-    deleteCookie,
-
-    Texture,
-    WXShare
-}
+export * from './observer/array.js';
+export * from './OS.js';
+export * from './util/index.js';
+export * from './URL.js';
+export * from "./Texture.js";
+export * from "./WX.js";
