56a0069e21052c38ed03c329bc212b8045b16438
bin.zeng <yy282371715@sina.cn>
2020-12-10 14:30:35 +0800

    update

--- src/utils/url.js
+++ src/utils/url.js
@@ -15,27 +15,29 @@ function parseURL (url) {
     host: a.hostname,
     port: a.port,
     query: a.search,
-    params: (function () {
-      var ret = {},
-        seg = a.search.replace(/^\?/, '').split('&'),
-        len = seg.length,
-        i = 0,
-        s;
-      for (; i < len; i++) {
-        if (!seg[i]) {
-          continue;
-        }
-        s = seg[i].split('=');
-        ret[s[0]] = s[1];
-      }
-      return ret;
-    })(),
+    params: parseQuery(a.search),
     file: (a.pathname.match(/([^/?#]+)$/i) || [, ''])[1],
     hash: a.hash.replace('#', ''),
     path: a.pathname.replace(/^([^/])/, '/$1'),
     relative: (a.href.match(/tps?:\/[^/]+(.+)/) || [, ''])[1],
     segments: a.pathname.replace(/^\//, '').split('/')
   };
+}
+
+function parseQuery (query) {
+  var ret = {},
+    seg = query.trim().replace(/^(\?|#|&)/, '').split('&'),
+    i = 0,
+    len = seg.length,
+    s;
+  for (; i < len; i++) {
+    if (!seg[i]) {
+      continue;
+    }
+    s = seg[i].split('=');
+    ret[s[0]] = s[1];
+  }
+  return ret;
 }
 
 /**
