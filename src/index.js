05e77bda82da3c7f18be8b9e3d747ef76b2ca923
bin.zeng <yy282371715@sina.cn>
2021-02-18 16:12:01 +0800

    update

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './native'
-import {version} from '../package.json'
-
-export {default as $} from './$'
-export {default as Texture} from './canvas/Texture'
-export {version}
--- src/native/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * 遵循mozilla的规则：扩展内置原型的唯一理由是支持JavaScript 引擎的新特性，如Array.forEach。
- */
-
-import './String'
-import './Array'
